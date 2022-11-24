d72a7cd5176d328584fd22439ccd5a1de8ae75e0
jaydeepIneuron007 <jaydeep@ineuron.ai>
2022-11-24 12:54:55 +0530

    docker added

--- clothing/logger/__init__.py
+++ clothing/logger/__init__.py
@@ -5,7 +5,7 @@ from from_root import from_root
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(from_root(), "logs", LOG_FILE)
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 
 os.makedirs(logs_path, exist_ok=True)
 
